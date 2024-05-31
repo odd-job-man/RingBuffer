c3e28b58caf54484db28268663c3665ea20c1e22
odd-job-man <dhtmdwls987@gmail.com>
2024-05-31 21:58:39 +0900

    ㅇ

--- RingBuffer/RingBuffer.cpp
+++ RingBuffer/RingBuffer.cpp
@@ -38,6 +38,7 @@ int RingBuffer::GetFreeSize(void)
 // Parameters:	IN (char *) Ring Buffer에 Data를 넣을 대상 Buffer
 //				IN (int) 복사할 크기
 // Return:		(int) Ring Buffer에 들어간 크기(사실상 0 혹은 sizeToPut 둘중 하나다)
+// 특이사항 : Enqueue정책은 sizeToPut() > GetFreeSize()이면 0을 반환한다.
 //--------------------------------------------------------------------
 int RingBuffer::Enqueue(IN char* pDest, IN size_t sizeToPut)
 {
@@ -64,7 +65,13 @@ int RingBuffer::Enqueue(IN char* pDest, IN size_t sizeToPut)
 	return firstSize + secondSize;
 }
 
-
+//--------------------------------------------------------------------
+// 기능: Ring Buffer에서 Data 꺼냄
+// Parameters:	OUT (char *) Ring Buffer의 Data를 받을 대상 Buffer
+//				IN (int) 복사할 크기
+// Return:		(int) Ring Buffer에서 꺼내서 pDest에 복사한 데이터의 크기
+// 특이사항 : Dequeue정책은 sizeToRead > GetUseSize()이면 Dequeue를 제대로 수행하지않고 0을 반환한다.
+//--------------------------------------------------------------------
 int RingBuffer::Dequeue(OUT char* pDest, IN size_t sizeToRead)
 {
 	int useSize = GetUseSize();
@@ -86,6 +93,7 @@ int RingBuffer::Dequeue(OUT char* pDest, IN size_t sizeToRead)
 	return firstSize + secondSize;
 }
 
+// 해당함수는 Dequeue와 거의 같지만 복사만 수행하고 front의 위치가 바뀌지 않는다. 
 int RingBuffer::Peek(IN int sizeToPeek, OUT char* pTarget)
 {
 	int useSize = GetUseSize();
@@ -110,6 +118,9 @@ void RingBuffer::ClearBuffer(void)
 	rear_ = front_;
 }
 
+
+// 기능 : rear_ + 1부터 큐의 맨앞으로 이동하지 않고 Enqueue가능한 최대 크기를 반환함.
+// 즉 rear+ 1부터 front_ - 1까지의 거리 혹은 rear_ + 1부터 
 int RingBuffer::DirectEnqueueSize(void)
 {
 	if (rear_ >= front_)
@@ -134,23 +145,27 @@ int RingBuffer::DirectDequeueSize(void)
 	}
 }
 
+// sizeToMove만큼 rear_를 이동시키고 현재 rear_를 반환함.
 int RingBuffer::MoveRear(IN int sizeToMove)
 {
 	rear_ = (rear_ + sizeToMove) % (BUFFER_SIZE + 1);
 	return rear_;
 }
 
+// sizeToMove만큼 front_이동시키고 현재 rear_를 반환함.
 int RingBuffer::MoveFront(IN int sizeToMove)
 {
 	front_ = (front_ + sizeToMove) % (BUFFER_SIZE + 1);
 	return front_;
 }
 
+// 현재 front_인덱스에 알맞는 포인터를 반환
 char* RingBuffer::GetFrontPtr(void)
 {
 	return pBuffer_ + front_;
 }
 
+// 현재 rear_ 인덱스에 알맞는 포인터를 반환
 char* RingBuffer::GetRearPtr(void)
 {
 	return pBuffer_ + rear_;
