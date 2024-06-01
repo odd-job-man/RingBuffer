126d492de5dae70324c1985cc81a24cc1737282d
odd-job-man <dhtmdwls987@gmail.com>
2024-06-01 12:50:46 +0900

    d

--- RingBuffer/RingBuffer.h
+++ RingBuffer/RingBuffer.h
@@ -2,7 +2,7 @@
 #define IN
 #define OUT
 
-constexpr int BUFFER_SIZE = 10000;
+constexpr int BUFFER_SIZE = 1000;
 
 class RingBuffer
 {
--- RingBuffer/main.cpp
+++ RingBuffer/main.cpp
@@ -7,36 +7,81 @@
 #include <WS2tcpip.h>
 #include "RingBuffer.h"
 #include <windows.h>
+#include <queue>
 constexpr int CONSOLE_WIDTH = 237;
 
+RingBuffer rbTemp;
 RingBuffer rb;
 char dequeueBuf[CONSOLE_WIDTH + 2];
-char peekBuf[CONSOLE_WIDTH + 2];
+char peekBuf[10000 + 2];
+
+char tempBuf[10000 + 2];
 
 void printArray(int width, char* pBuf) {
     for (int i = 0; i < width; ++i) {
 		printf("%c", pBuf[i]);
     }
 }
+
+// 큐에서 원하는 바이트 수만큼 추출하여 임의의 버퍼에 집어넣는 함수
+void extractBytes(std::queue<char>& myQueue, char* buffer, int bytes) {
+	int index = 0;
+	while (bytes > 0 && !myQueue.empty()) {
+		buffer[index++] = myQueue.front();
+		myQueue.pop();
+		--bytes;
+	}
+}
+
 int main()
 {
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	int consoleWidth = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 	char testString[] = "@234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345*!";
-	srand(6);
+	int len = strlen(testString);
+	std::queue<char> q;
+	srand((unsigned)time(nullptr));
 	while (true)
 	{
-		int randBufSize = rand() % 239;
-		int enqueueSize = rb.Enqueue(testString, sizeof(testString) - 1);
-		int remainBufSize = CONSOLE_WIDTH - randBufSize;
-		char* pTemp = rb.pBuffer_ + rb.front_ + 1;
 		memset(peekBuf, 0, sizeof(peekBuf));
 		memset(dequeueBuf, 0, sizeof(dequeueBuf));
-		int PeekSize = rb.Peek(randBufSize, peekBuf);
-		//printf(peekBuf);
-		int dequeueSize = rb.Dequeue(dequeueBuf, randBufSize);
-		printf(dequeueBuf);
+		memset(tempBuf, 0, sizeof(tempBuf));
+
+		int enqueueSize = 0;
+		int dequeueSize = 0;
+
+		for (int i = 0; i < len; ++i)
+		{
+			q.push(testString[i]);
+		}
+		
+		int qSize = q.size();
+		int rbSize = rb.GetFreeSize();
+		if (rbSize == 0)
+		{
+			dequeueSize = rb.Dequeue(dequeueBuf, rand() % rb.GetUseSize() + 1);
+			printArray(dequeueSize, dequeueBuf);
+			continue;
+		}
+
+		int RandomByteToExtract = rand() % (qSize > rbSize ? rbSize : qSize) + 1;
+
+		int index = 0;
+		extractBytes(q, tempBuf, RandomByteToExtract);
+		enqueueSize = rb.Enqueue(tempBuf, RandomByteToExtract);
+		dequeueSize = rb.Dequeue(dequeueBuf, rand() % RAND_MAX + 1);
+		if(dequeueSize > 0)
+			printArray(dequeueSize, dequeueBuf);
+
+		if (q.size() > 100000)
+		{
+			memset(dequeueBuf, 0, sizeof(dequeueBuf));
+			dequeueSize = rb.Dequeue(dequeueBuf, rb.GetUseSize());
+			printArray(dequeueSize, dequeueBuf);
+			while (q.size() > 0)
+			{
+				putchar(q.front());
+				q.pop();
+			}
+		}
 	}
 }
 
